6126dd73ccb3a50025b1c0c2f988739a42ea3f2d
Sri Hari Acha <srihari.acha@emirates.com>
2020-12-24 15:09:50 +0400

    added post reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-import success from './success.reducer';
+import posts from './posts/reducer';
 
 
 export default combineReducers({
-    success,
+    posts,
 });
